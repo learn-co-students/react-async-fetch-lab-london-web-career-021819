6ffdbbad9e3748453c9cf5635729fe63d98864ed
github username <github email address>
2019-04-28 18:25:10 +0100

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,32 @@
+import React, { Component } from "react";
 // create your App component here
+URL = "http://api.open-notify.org/astros.json";
+
+class App extends Component {
+  state = {
+    astrosInSpace: []
+  };
+
+  componentDidMount(){
+    fetch(URL)
+      .then(response => response.json())
+      .then(json => this.setState({ astrosInSpace: json.people }));
+  };
+  render() {
+    return (
+      <div>
+        <h2>
+          There are {this.state.astrosInSpace.length} astronauts in space RIGHT
+          NOW
+        </h2>
+        <ul>
+          {this.state.astrosInSpace.map(astro => (
+            <li>{astro.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
